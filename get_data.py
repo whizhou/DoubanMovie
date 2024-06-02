a3d8910cc8913d123f4fe72ba9a7733183ccb914
Fuzzhou <1822598530@qq.com>
2024-06-02 11:28:05 +0800

    Add scriptwriters, actors, types, regions, languages

--- get_data.py
+++ get_data.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 
 # 下载网页
@@ -29,7 +30,7 @@ def getHtml(url) -> requests.Response:
 
 
 # TODO 爬取单个电影信息
-def getMovie(url) -> pd.Series:
+def getMovie(url) -> list:
     """
     爬取单个电影的信息（除短评）
     :param url:
@@ -42,21 +43,59 @@ def getMovie(url) -> pd.Series:
     name = bs.find('span', property='v:itemreviewed').text
 
     year = bs.find('span', class_='year').text
+    year = int(year[1:-1])  # str2num '(1999)' -> 1999
     # print("%s (%d)" % (name, year))
 
     # info = bs.find_all('div', id='info')
     director = bs.find('a', rel="v:directedBy").text
 
     writer_span = bs.find('span', string='编剧')
-    writers = writer_span.find_next('span', class_='attrs').find_all('a')
-    scriptwriters = [writer.get_text() for writer in writers]
-    print(scriptwriters)
-    # scriptwriters = info.find('a', )
-    # print(info)
+    writers_set = writer_span.find_next('span', class_='attrs').find_all('a')
+    scriptwriters = [writer.get_text() for writer in writers_set]
+    # 转换为字符串列表
+    # print(scriptwriters)
 
+    actor_set = bs.find_all('a', rel='v:starring')
+    actors = [actor.get_text() for actor in actor_set]
+    # 转换为字符串列表
+    # print(actors)
+
+    type_set = bs.find_all('span', property='v:genre')
+    types = [movie_type.get_text() for movie_type in type_set]
+    # print(types)
+
+    region_span = bs.find('span', string='制片国家/地区:')
+    region_set = region_span.next_sibling
+    regions = [region.strip() for region in region_set.split('/')]
+    # print(regions)
+
+    language_span = bs.find('span', string='语言:')
+    language_set = language_span.next_sibling
+    languages = [language.strip() for language in language_set.split('/')]
+    # print(languages)
+
+    date_set = bs.find_all('span', property='v:initialReleaseDate')
+    dates_locations = [date.get_text() for date in date_set]
+    dates = [] # 初始化 dates 空列表
+    for item in dates_locations:
+        # 找到左右括号位置
+        left_paren_index = item.find('(')
+        right_paren_index = item.find(')')
+
+        # 提取日期字符串并装换为 datetime 对象
+        date_str = item[:left_paren_index]
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+
+        # 提取地点字符串
+        location = item[left_paren_index + 1:right_paren_index]
+
+        # 添加到 dates 嵌套列表
+        dates.append([date, location])
+    # print(dates)
 
 
 # TODO 爬取所有电影信息并保存到 data.csv
+
 
 test_urls = [
     'https://movie.douban.com/subject/1292052/',
@@ -64,6 +103,7 @@ test_urls = [
     'https://movie.douban.com/subject/1292720/',
     'https://movie.douban.com/subject/1292722/',
     'https://movie.douban.com/subject/1291561/',
+    'https://movie.douban.com/subject/1292063/'
 ]
 for test_url in test_urls:
     getMovie(test_url)
